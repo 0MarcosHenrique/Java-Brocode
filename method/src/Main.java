82d93d5b1c94fc118a4e14dafed5966e1b8739d6
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-02-21 10:20:13 -0300

    Aprendi muito

--- method/src/Main.java
+++ method/src/Main.java
@@ -1,15 +1,38 @@
 public class Main {
     public static void main(String[] args){
 
-        String name = "Marcos";
-        int age = 18;
+    int age = 16;
 
-        felizAniversario(name, age);
+    if (ageCheck(age)){
+        System.out.println("Você pode se inscrever");
+        }
+    else {
+        System.out.println("Você não pode se inscrever");
     }
-    static void felizAniversario(String name, int age){
-        System.out.println("Parabens pra você!!!");
-        System.out.printf("Parabens pro %s!\n", name);
-        System.out.printf("Voce esta fazendo %d anos de idade\n", age);
-        System.out.println("Feliz aniversario\n");
+
+    }
+
+    static void felizAniversario(String name, int age){ }
+
+    static double square(double number) {
+        return number * number;
+    }
+
+    static double cube (double number) {
+        return number * number * number;
+    }
+
+    static String pegarNomeCompleto (String first, String last){
+        return first + " " + last;
+    }
+
+    static boolean ageCheck(int age){
+        if(age >= 18){
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 }
